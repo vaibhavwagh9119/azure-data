b0b75245a6557493cc2ac71cf808649caf676376
vaibhavwagh9119 <vaibhavwagh9119@gmail.com>
2025-12-03 14:46:27 +0530

    changes

--- terra/main.tf
+++ terra/main.tf
@@ -17,13 +17,8 @@ module "data_factory" {
   resource_group_name = var.resource_group_name
   identity_type = var.identity_type
   identity_ids  = var.identity_ids
-  repo_configuration     = var.repo_configuration
-  customer_managed_key   = var.customer_managed_key
   encryption             = var.encryption
   global_parameters      = var.global_parameters
   public_network_enabled = var.public_network_enabled
   tags                   = var.tags
-}
-output "data_factory_id" {
-  value = module.data_factory.id
 }
--- terra/modules/data_factory/main.tf
+++ terra/modules/data_factory/main.tf
@@ -8,29 +8,8 @@ resource "azurerm_data_factory" "Kdev" {
     identity_ids = var.identity_ids
   }
   // ---------- Repo Configuration ----------
-  dynamic "repo_configuration" {
-    for_each = var.repo_configuration == null ? [] : [var.repo_configuration]
-    content {
-      type                 = repo_configuration.value.type
-      account_name         = repo_configuration.value.account_name
-      repository_name      = repo_configuration.value.repository_name
-      collaboration_branch = repo_configuration.value.collaboration_branch
-      root_folder          = repo_configuration.value.root_folder
-      project_name         = repo_configuration.value.project_name
-      tenant_id            = repo_configuration.value.tenant_id
-    }
-  }
 
 
-  // ---------- Encryption ----------
-  dynamic "encryption" {
-    for_each = var.encryption == null ? [] : [var.encryption]
-    content {
-      vault_base_url = encryption.value.vault_base_url
-      key_name       = encryption.value.key_name
-      key_version    = encryption.value.key_version
-    }
-  }
   // ---------- Global Parameters ----------
   dynamic "global_parameter" {
     for_each = var.global_parameters
